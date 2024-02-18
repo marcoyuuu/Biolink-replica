fc0352071ca665e0b31354bf088927263bd2398f
marcoyuuu <yu.marco7@gmail.com>
2024-02-17 21:31:25 -0400

    test movil

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+// Define variables globales para el título de la página y el año actual
 $tituloPagina = "Red Sísmica de Puerto Rico";
 $añoActual = date("Y");
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -11,90 +13,127 @@ $añoActual = date("Y");
     <title><?php echo $tituloPagina; ?></title>
     <link rel="stylesheet" href="style.css">
     <script src="scripts.js" defer></script>
+    <link rel="shortcut icon" type="image/jpg" href="assets/logo.png"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <style>
+
+    </style>
 </head>
 
 <body>
 
+    <!-- Contenedor Principal: Agrupa todo el contenido de la página -->
     <div class="contenedor-principal">
+        <!-- Cabezera: Contiene elementos de identificación y navegación -->
         <header>
-            <button id="openModalBtn" class="boton-compartir">Compartir Esta Página</button>
+            <!-- Botón para abrir el modal de compartir -->
+            <button id="botonCompartir" class="boton-compartir"><img src="assets\share.png" alt="Compartir"></button>
+            <!-- Logo, título y subtítulo de la página -->
             <img src="assets/logo.png" class="logo" alt="Logo de la Red Sísmica de Puerto Rico">
             <h1 class="page-title">Red Sísmica de Puerto Rico</h1>
             <h2 class="page-bioline">Bienvenido a la Red Sísmica de Puerto Rico. Este sitio ofrece información y recursos relacionados con la sismología en Puerto Rico.</h2>
         </header>
 
+        <!-- Contenido Principal: Sección para el contenido específico de la página -->
         <main>
-            <!-- Iconos/enlaces -->
+            <!-- Contenedor de Iconos: Enlaces directos a redes sociales y contacto -->
             <div class="contenedor-icono">
-                <a href="mailto:charlas@prsnmail.uprm.edu"><img src="assets\mail.png" alt="Mail"></a>
-                <a href="https://www.facebook.com/redsismicadepuertorico"><img src="assets\facebook.png" alt="Facebook"></a>
-                <a href="tel:+17878338433"><img src="assets\phone.png" alt="Phone"></a>
-                <a href="https://instagram.com/redsismicapr"><img src="assets\instagram.png" alt="Instagram"></a>
-                <a href="https://www.youtube.com/redsismicapr"><img src="assets\youtube.png" alt="YouTube"></a>
-                <a href="https://twitter.com/redsismica"><img src="assets\x.png" alt="Twitter"></a>
+                <a href="mailto:charlas@prsnmail.uprm.edu" target="_blank"><img src="assets\mail.png" alt="Mail"></a>
+                <a href="https://www.facebook.com/redsismicadepuertorico" target="_blank"><img src="assets\facebook.png" alt="Facebook"></a>
+                <a href="tel:+17878338433" target="_blank"><img src="assets\phone.png" alt="Phone"></a>
+                <a href="https://instagram.com/redsismicapr" target="_blank"><img src="assets\instagram.png" alt="Instagram"></a>
+                <a href="https://www.youtube.com/redsismicapr" target="_blank"><img src="assets\youtube.png" alt="YouTube"></a>
+                <a href="https://twitter.com/redsismica" target="_blank"><img src="assets\x.png" alt="Twitter"></a>
             </div>
         </main>
 
-        <!-- Botones -->
+        <!-- Botones de Enlaces: Proporciona enlaces a recursos y herramientas ofrecidas por la Red Sísmica -->
         <div class="contenedor-botones">
-            <a href="https://www.tsunamizone.org/puertorico/" class="boton">
+            <a href="https://www.tsunamizone.org/puertorico/" class="boton terremoto" target="_blank">
                 <img src="assets/cwave.png" alt="Icon">
-                <span class="item-title">Caribe Wave</span>
+                <span class="titulo-item">Caribe Wave</span>
             </a>
-            <a href="https://redsismica.uprm.edu/" class="boton">
+            <a href="https://redsismica.uprm.edu/" class="boton" target="_blank">
                 <img src="assets/princ.png" alt="Icon">
-                <span class="item-title">Página principal</span>
+                <span class="titulo-item">Página principal</span>
             </a>
-            <a href="https://redsismica.uprm.edu/spanish/educacion/solicitud_conferencia.php" class="boton">
+            <a href="https://redsismica.uprm.edu/spanish/educacion/solicitud_conferencia.php" class="boton" target="_blank">
                 <img src="assets/char.png" alt="Icon">
-                <span class="item-title">Solicitud de Charlas</span>
+                <span class="titulo-item">Solicitud de Charlas</span>
             </a>
-            <a href="https://redsismica.uprm.edu/spanish/educacion/recursos_educativos.php" class="boton">
+            <a href="https://redsismica.uprm.edu/spanish/educacion/recursos_educativos.php" class="boton" target="_blank">
                 <img src="assets/edu.png" alt="Icon">
-                <span class="item-title">Recursos Educativos</span>
+                <span class="titulo-item">Recursos Educativos</span>
             </a>
-            <a href="https://redsismica.uprm.edu/spanish/tsunami/" class="boton">
+            <a href="https://redsismica.uprm.edu/spanish/tsunami/" class="boton" target="_blank">
                 <img src="assets/tsu.png" alt="Icon">
-                <span class="item-title">Portal de tsunami</span>
+                <span class="titulo-item">Portal de tsunami</span>
             </a>
-            <a href="https://prddst.uprm.edu/" class="boton">
+            <a href="https://prddst.uprm.edu/" class="boton" target="_blank">
                 <img src="assets/maptool.png" alt="Icon">
-                <span class="item-title">Map tool</span>
+                <span class="titulo-item">Map tool</span>
             </a>
-            <a href="https://redsismica.uprm.edu/spanish/tsunami/mapas_desalojo.php" class="boton">
+            <a href="https://redsismica.uprm.edu/spanish/tsunami/mapas_desalojo.php" class="boton" target="_blank">
                 <img src="assets/map.png" alt="Icon">
-                <span class="item-title">Mapas de Desalojo</span>
+                <span class="titulo-item">Mapas de Desalojo</span>
             </a>
-            <a href="https://www.shakeout.org/puertorico/" class="boton">
+            <a href="https://www.shakeout.org/puertorico/" class="boton" target="_blank">
                 <img src="assets/shake.png" alt="Icon">
-                <span class="item-title">Shake Out</span>
+                <span class="titulo-item">Shake Out</span>
             </a>
 
         </div>
 
-        <div id="shareModal" class="modal">
-            <div class="contenido-modal">
-                <span id="closeModalBtn" class="cerrar-modal">&times;</span>
-                <h2>Compartir esta página</h2>
-                <h2>Compartir esta página</h2>
-                <!-- Placeholders for sharing options -->
-                <div>Twitter, Facebook, Email buttons here</div>
-                <!-- Placeholder for QR Code -->
-                <div id="qrCode"></div>
-                <!-- Placeholder for copy link feature -->
-                <div id="copyLinkContainer">
-                    <button id="copyLinkBtn">Copy Link</button>
-                    <p id="pageLink">Your Page URL here</p>
+        <!-- Modal: Estructura para compartir información de la página -->
+        <div id="modalCompartir" class="modal">
+            <div class="contenidoModal">
+                <!-- Botón para cerrar el modal, título del modal, y estructura para compartir vía redes sociales o copiar enlace -->
+                <span id="botonCerrar" class="cerrarModal">&times;</span>
+                <h1 class="tituloModal">Compartir esta página</h1>
+                <div class="flexContenedor">
+                    <div class="contenedorQR">
+                        <!-- Imagen del código QR y botón de descarga -->
+                        <img src="assets/qr-code.png" class="codigoQR" alt="Código QR">
+                        <button class="descargarQR">Descargar</button>
+                    </div>
+                    <div class="opcionesCompartir">
+                        <!-- Compartir en Twitter -->
+                        <a href="https://twitter.com/intent/tweet?text=Check%20out%20Red%20S%C3%ADsmica%20de%20Puerto%20Rico%27s%20bio%20link!%20%0a%0ahttps://bio.link/redsismica" class="botonSharePg twitter" target="_blank">
+                            <img src="assets/twitter.png" alt="Twitter">
+                            <span class="textoCompartir">Compartir en Twitter</span>
+                        </a>
+
+                        <!-- Compartir en Facebook -->
+                        <a href="https://www.facebook.com/sharer/sharer.php?u=https://bio.link/redsismica" class="botonSharePg facebook" target="_blank">
+                            <img src="assets/facebook2.png" alt="Facebook">
+                            <span class="textoCompartir">Compartir en Facebook</span>
+                        </a>
+
+                        <!-- Compartir por Email -->
+                        <a href="mailto:?subject=Check out this Biolink!&body=Check out Red Sísmica de Puerto Rico's bio link! - https://bio.link/redsismica" class="botonSharePg email" target="_blank">
+                            <img src="assets/mail.png" alt="Mail">
+                            <span class="textoCompartir">Compartir en Email</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="enlaceCopiar">
+                    <i class="fas fa-link"></i> <!-- Ícono de enlace -->
+                    <input type="text" value="https://bio.link/redsisimica" readonly>
+                    <button id="botonCopiar">Copiar</button>
                 </div>
             </div>
         </div>
 
+
+
+        <!-- Pie de Página: Contiene derechos de autor con año dinámico -->
         <footer>
-            <p>&copy; <?php echo $añoActual; ?> Red Sísmica de Puerto Rico</p>
+            <p>Copyright &copy; Red Sísmica de Puerto Rico <?php echo $añoActual; ?></p>
         </footer>
     </div>
 
     <script src="scripts.js"></script>
+
 </body>
 
 </html>
--- scripts.js
+++ scripts.js
@@ -1,29 +1,69 @@
 // Espera a que el DOM esté completamente cargado
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtener el botón que abre el modal
+    var btnAbrir = document.getElementById('botonCompartir');
+    // Obtener el modal
+    var modal = document.getElementById('modalCompartir');
+    // Obtener el botón que cierra el modal
+    var btnCerrar = document.getElementById('botonCerrar');
+    // Obtener el botón que copia el enlace al portapapeles
+    var botonCopiar = document.getElementById('botonCopiar');
+    // Obtener el input que contiene el enlace a copiar
+    var enlaceInput = document.querySelector('.enlaceCopiar input');
 
-  // Función para abrir el modal de compartir
-  function abrirModal() {
-      document.getElementById('shareModal').style.display = 'block';
-  }
+    // Función para abrir el modal de compartir
+    function abrirModal() {
+        modal.style.display = 'flex'; // Muestra el modal
+        document.body.style.overflow = 'hidden'; // Deshabilita el desplazamiento del fondo
+    }
 
-  // Función para cerrar el modal de compartir
-  function cerrarModal() {
-      document.getElementById('shareModal').style.display = 'none';
-  }
+    // Función para cerrar el modal de compartir
+    function cerrarModal() {
+        modal.style.display = 'none'; // Oculta el modal
+        document.body.style.overflow = 'auto'; // Habilita el desplazamiento del fondo
+    }
 
-  // Cierra el modal si se hace clic fuera de él
-  function cerrarModalExterno(event) {
-      if (event.target == document.getElementById('shareModal')) {
-          cerrarModal();
-      }
-  }
+    // Event listener para abrir el modal
+    if (btnAbrir) {
+        btnAbrir.addEventListener('click', abrirModal);
+    }
 
-  // Evento para abrir el modal
-  document.getElementById('openModalBtn').addEventListener('click', abrirModal);
+    // Event listener para cerrar el modal al hacer clic en el botón de cierre
+    if (btnCerrar) {
+        btnCerrar.addEventListener('click', cerrarModal);
+    }
 
-  // Evento para cerrar el modal con el botón de cerrar
-  document.getElementById('closeModalBtn').addEventListener('click', cerrarModal);
+    // Event listener para cerrar el modal al hacer clic fuera de él
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            cerrarModal();
+        }
+    });
 
-  // Evento para cerrar el modal haciendo clic fuera de él
-  window.addEventListener('click', cerrarModalExterno);
+    // Copia el enlace al portapapeles
+    botonCopiar.addEventListener('click', function () {
+        navigator.clipboard.writeText(enlaceInput.value).then(function () {
+            // Cambia el texto del botón a "¡Copiado!"
+            botonCopiar.textContent = "¡Copiado!";
+
+            // Espera 1 segundo antes de cambiar el texto de vuelta a "Copiar"
+            setTimeout(() => {
+                botonCopiar.textContent = "Copiar";
+            }, 1000); // 1000 milisegundos = 1 segundo
+        }).catch(err => {
+            console.error('Error al copiar el texto: ', err);
+        });
+    });
+
+    // Descarga código QR al presionar botón de descarga
+    document.querySelector('.descargarQR').addEventListener('click', function () {
+        var a = document.createElement('a');
+        a.href = 'assets/qr-code.png'; // Ruta al archivo que deseas descargar
+        a.download = 'qr-code.png'; // Nombre del archivo descargado
+        document.body.appendChild(a); // Agrega el enlace al documento
+        a.click(); // Simula un clic en el enlace
+        document.body.removeChild(a); // Elimina el enlace del documento
+    });
+
+
 });
